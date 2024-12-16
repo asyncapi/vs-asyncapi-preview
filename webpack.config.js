0a8cfbfc31b677f052e36a174df4d95cf4bfc4ba
Milan D Vijay <153212508+ibishal@users.noreply.github.com>
2024-12-16 15:59:51 +0530

    feat: completed the integration of applicationView (#242)
    Co-authored-by: Iván García Sainz-Aja <ivangsa@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,14 @@ const extensionConfig = {
           from: 'node_modules/@asyncapi/react-component/styles/default.min.css',
           to: 'node_modules/@asyncapi/react-component/styles/default.min.css',
         },
+        {
+          from: 'node_modules/@asyncapi/edavisualiser/browser/standalone/index.js',
+          to: 'node_modules/@asyncapi/edavisualiser/browser/standalone/index.js',
+        },
+        {
+          from: 'node_modules/@asyncapi/edavisualiser/styles/default.min.css',
+          to: 'node_modules/@asyncapi/edavisualiser/styles/default.min.css',
+        },
       ],
     }),
   ],
