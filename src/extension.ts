62e38e140e54dff8ee811eb86f0403934fd9f439
ROHITH YARRAMALA <91138500+rohithyarramala@users.noreply.github.com>
2025-04-24 16:25:49 +0530

    removed unwanted console logs

--- src/extension.ts
+++ src/extension.ts
@@ -34,7 +34,6 @@ export function activate(context: vscode.ExtensionContext) {
       setAsyncAPIPreviewContext(vscode.window.activeTextEditor.document);
     }
     if (isAsyncAPIFile(document)) {
-      console.log('File saved. Triggering visualizer reload for', document.uri.fsPath);
       visualizeAsyncApi(context)(document.uri); // Automatically reload the visualizer for the file
     }
   });
